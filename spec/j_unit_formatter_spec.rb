cf00dbfe39953e417447e8720ac8cdf0835732aa
Mark Wolfe <mark@wolfe.id.au>
2011-11-21 09:23:06 +1100

    Tidy up test as it sucked, a lot.

--- spec/j_unit_formatter_spec.rb
+++ spec/j_unit_formatter_spec.rb
@@ -14,24 +14,16 @@ describe JUnitFormatter do
 
   end
 
-  describe 'XML format' do
+  it 'should match the sample header two tests' do
 
-    describe 'summary section' do
+    @formatter.start_dump
+    @formatter.dump_summary(0.00001, 2, 0, 0)
 
-      it 'should match the sample for two tests' do
-
-        @formatter.start_dump
-        @formatter.dump_summary(0.00001, 2, 0, 0)
-
-        @output.string.should eql('<?xml version="1.0" encoding="UTF-8"?>
+    @output.string.should eql('<?xml version="1.0" encoding="UTF-8"?>
 <testsuite tests="2" failures="0" errors="0" time="0.000010" timestamp="2011-01-01T00:00:00Z">
   <properties/>
 </testsuite>
 ')
-      end
-
-    end
-
   end
 
 end
